07bff40768c4accf8e3f9c3aa0e5e81e4f1fce60
aman4991 <66277758+aman4991@users.noreply.github.com>
2020-07-16 13:31:00 -0400

    Update SQLLiteDatabaseHelper.java
    table names for notes and notes table query

--- app/src/main/java/com/NoteGVAandroid/SQLLiteDatabaseHelper.java
+++ app/src/main/java/com/NoteGVAandroid/SQLLiteDatabaseHelper.java
@@ -17,7 +17,24 @@ public class SQLLiteDatabaseHelper extends SQLiteOpenHelper {
             CATEGORY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             CATEGORY_NAME + " TEXT NOT NULL );";
 
-  
+   //Create Notes Table Query
+    private static final String CREATE_NOTES_TABLE = "create table " +
+            NOTES_TABLE_NAME + " (" +
+            NOTES_ID + " INTEGER , " +
+            TITLE + " TEXT NOT NULL, " +
+            DATE + " TEXT NOT NULL, " +
+            DESCRIPTION + " TEXT, " +
+            AUDIO + " TEXT, " +
+            LOCATION + " TEXT, " +
+            NOTES_CATEGORY_ID + " INTEGER NOT NULL); ";
+
+    // Images Table Name
+    public static final String NOTES_TABLE_IMAGES = "IMAGES";
+
+    public static final String IMAGE = "image";
+    public static final String NOTES_IMAGE_ID = "notes_id";
+    public static final String IMAGE_CATEGORY_ID = "category_id";
+
   
   
 }
