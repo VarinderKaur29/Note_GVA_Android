b951a9f84f5f5762fe8978006933d7671f6099d4
aman4991 <66277758+aman4991@users.noreply.github.com>
2020-07-16 13:03:16 -0400

    Update SQLLiteDatabaseHelper.java
    add category Model for table name , table column and table query

--- app/src/main/java/com/NoteGVAandroid/SQLLiteDatabaseHelper.java
+++ app/src/main/java/com/NoteGVAandroid/SQLLiteDatabaseHelper.java
@@ -1,4 +1,23 @@
 package com.NoteGVAandroid;
 
-public class SQLLiteDatabaseHelper {
+public class SQLLiteDatabaseHelper extends SQLiteOpenHelper {
+    static final String DATABASE_NAME = "NOTES.DB";
+    static final int DB_VERSION = 1;
+
+    //CategoryModel Table Name
+    public static final String CATEGORY_TABLE_NAME = "CATEGORIES";
+
+    //CategoryModel Table Column
+    public static final String CATEGORY_ID = "category_id";
+    public static final String CATEGORY_NAME = "category_name";
+
+    // Create CategoryModel Table Query
+    private static final String CREATE_CATEGORY_TABLE = "create table " +
+            CATEGORY_TABLE_NAME + "(" +
+            CATEGORY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            CATEGORY_NAME + " TEXT NOT NULL );";
+
+  
+  
+  
 }
