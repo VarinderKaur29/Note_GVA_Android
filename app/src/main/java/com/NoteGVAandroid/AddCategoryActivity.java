1d3c56785461cb319de71f42ed4cd7ecfc3b91c1
VarinderKaur29 <66277503+VarinderKaur29@users.noreply.github.com>
2020-07-17 00:57:15 -0400

    Update AddCategoryActivity.java
    add category class (Varinder kaur)

--- app/src/main/java/com/NoteGVAandroid/AddCategoryActivity.java
+++ app/src/main/java/com/NoteGVAandroid/AddCategoryActivity.java
@@ -1,14 +1,45 @@
 package com.NoteGVAandroid;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AddCategoryActivity extends AppCompatActivity {
+
+    //Variables
+    EditText category_et;
+    String category_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_category);
+    }
+
+    public void finish(View view) {
+        finish();
+    }
+
+    public void add_category(View view) {
+        initData();
+
+    }
+    private void initData() {
+        category_et = findViewById(R.id.category_name);
+        category_name = category_et.getText().toString().trim();
+        if (category_name.equalsIgnoreCase("")) {
+            Toast.makeText(AddCategoryActivity.this, "Enter CategoryModel", Toast.LENGTH_SHORT).show();
+        } else {
+            DatabaseManager db = new DatabaseManager(AddCategoryActivity.this);
+            db.open();
+            db.insertCategory(category_name);
+            db.close();
+            Toast.makeText(AddCategoryActivity.this, "CategoryModel Added Successfully", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
+
